8be8ec0e02977de321c7af602d7f5c3ecfc1549a
nada <nadaalsayyad4@gmail.com>
2024-11-13 10:11:25 +0200

    + Hadith details

--- lib/my_app/my_app.dart
+++ lib/my_app/my_app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:islami/config/theme/my_theme.dart';
 import 'package:islami/core/routes_manager.dart';
+import 'package:islami/presentation/screens/hadith_details_screen/hadith_details_screen.dart';
 
 import '../presentation/screens/home/home_screen.dart';
 import '../presentation/screens/quran_details_screen/quran_details_screen.dart';
-import '../presentation/splash/splash_screen.dart';
+import '../presentation/screens/splash/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         RoutesManager.splashRoute: (_) => const SplashScreen(),
         RoutesManager.homeRoute: (_) => HomeScreen(),
         RoutesManager.quranDetailsRoute: (_) => QuranDetailsScreen(),
+        RoutesManager.hadithDetailsRoute: (_) => HadithDetailsScreen(),
       },
       initialRoute: RoutesManager.splashRoute,
     );
